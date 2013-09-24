99ff152d90414190fb7a9245df550840602a5eee
Anushan Easwaramoorthy <anushan.easwaramoorthy@xe.com>
2013-09-24 12:23:59 -0400

    - Allowing one extra step when no forward jumping is enabled. This will enable the anchor tag above the wizard for the next step, functioning just like hitting the next button.

--- js/jquery.smartWizard.js
+++ js/jquery.smartWizard.js
@@ -277,7 +277,7 @@ function SmartWizard(target, options) {
         }
         if ($this.options.noForwardJumping) {
             // +2 == +1 (for index to step num) +1 (for next step)
-            for (var i = $this.curStepIdx + 2; i <= $this.steps.length; i++) {
+            for (var i = $this.curStepIdx + 3; i <= $this.steps.length; i++) {
                 $this.disableStep(i);
             }
         }
